def42d71d8107e88d4b786f5100ff67297fba6b8
artem.korovin <ak.denisovich@gmail.com>
2021-09-14 20:46:47 +0300

    2nd task finished

--- tzdart/bin/tzdart.dart
+++ tzdart/bin/tzdart.dart
@@ -1,16 +1,31 @@
 import 'dart:io';
 import 'dart:math';
+
 void main() {
   var rng = Random();
   var massiv = List.generate(10, (int i) => rng.nextInt(500));
   var massiv2 = [];
-  for (int i in massiv){
-    if (i%2==0){
+  for (int i in massiv) {
+    if (i % 2 == 0) {
       massiv2.add(i);
     }
   }
-  
-   print(massiv);
-   print(massiv2);
+  print('TASK NUMBER ONE');
+  print('Random generated array - $massiv');
+  print('Even elements of random generated array - $massiv2'); //task 1 finished
+  print('Task number one finished');
 
+  print('');
+  print('TASK NUMBER TWO ');
+  while (true) {
+    print('Vvedite chislo ');
+    dynamic n = stdin.readLineSync()!;
+    if (int.tryParse(n) == null) {
+      print('Vi vveli ne chislo');
+    } else {
+      print('Vi vveli $n');
+    }
+  }
+
+  //task 2 finished
 }
