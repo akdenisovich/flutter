f0d64921f93810a6c42a4d83832e48a83adc6b15
artem.korovin <ak.denisovich@gmail.com>
2021-09-15 18:58:33 +0300

    3rd task finished

---
+++ tzdart3/bin/tzdart3.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  var massiv = [];
+  var l ;
+  String x ='';
+  while (x !='exit') {
+    print('Vvedite chislo ');
+    dynamic n = stdin.readLineSync()!;
+    x = n.toString();
+    if (int.tryParse(n) == null) {
+      print('Vi vveli ne chislo');
+      print('Dlina massiva $l');
+      
+    } else {
+      massiv.add(int.tryParse(n));
+      l= massiv.length;
+      print('Dlina massiva $l');
+    }
+  }
+ 
+}
